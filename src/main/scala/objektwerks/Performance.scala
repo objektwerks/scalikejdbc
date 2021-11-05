9d2c811c3d1eb4c46da027beef1f015419b83740
objektwerks <objektwerks@runbox.com>
2021-11-05 12:17:03 -0400

    added updateTodo benchmark to Performance

--- src/main/scala/objektwerks/Performance.scala
+++ src/main/scala/objektwerks/Performance.scala
@@ -3,11 +3,12 @@ package objektwerks
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 
+import java.util.UUID
 import java.util.concurrent.TimeUnit
 
 import org.openjdk.jmh.annotations._
 
-object Peformance extends LazyLogging {
+object Performance extends LazyLogging {
   val conf = ConfigFactory.load("app.conf")
   val store = Store(conf)
   logger.info("Database and Store initialized for performance testing.")
@@ -20,10 +21,20 @@ object Peformance extends LazyLogging {
 @Measurement(iterations = 10)
 @Fork(1)
 class Performance() {
-  import Peformance.store
+  import Performance.store
+
+  var todo = Todo(task = "todo")
+  val id = store.addTodo(todo)
+  todo = todo.copy(id = id)
 
   @Benchmark
-  def addTodo(): Int = store.addTodo(Todo(task = "todo"))
+  def addTodo(): Int = store.addTodo(Todo(task = UUID.randomUUID.toString))
+
+  @Benchmark
+  def updateTodo(): Unit = {
+    todo = todo.copy(task = UUID.randomUUID.toString)
+    store.updateTodo(todo)
+  }
 
   @Benchmark
   def listTodos(): Seq[Todo] = store.listTodos()
