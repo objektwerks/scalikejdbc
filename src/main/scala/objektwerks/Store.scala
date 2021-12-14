4fe1e2f1564a6881191ec1f17f8b5bc4a7876f53
objektwerks <objektwerks@runbox.com>
2021-12-14 08:32:36 -0500

    reformatted store

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -1,13 +1,13 @@
 package objektwerks
 
 import com.typesafe.config.Config
+
 import scalikejdbc._
 
-object Store {
+object Store:
   def apply(conf: Config): Store = new Store(conf)
-}
 
-class Store(conf: Config) {
+class Store(conf: Config):
   val driver = conf.getString("db.driver")
   val url = conf.getString("db.url")
   val user = conf.getString("db.user")
@@ -16,24 +16,20 @@ class Store(conf: Config) {
   Class.forName(driver)
   ConnectionPool.singleton(url, user, password)
 
-  def addTodo(todo: Todo): Int = {
+  def addTodo(todo: Todo): Int =
     DB localTx { implicit session =>
       sql"insert into todo(task) values(${todo.task})".update()
     }
-  }
 
-  def updateTodo(todo: Todo): Unit = {
+  def updateTodo(todo: Todo): Unit =
     DB localTx { implicit session =>
       sql"update todo set task = ${todo.task} where id = ${todo.id}".update()
     }
     ()
-  }
 
-  def listTodos(): Seq[Todo] = {
+  def listTodos(): Seq[Todo] =
     DB readOnly { implicit session =>
       sql"select * from todo"
         .map(rs => Todo( rs.int("id"), rs.string("task") ) )
         .list()
     }
-  }
-}
