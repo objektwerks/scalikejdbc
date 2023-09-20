78b6fb863fe0495e62048d01948b02812f3b9c64
objektwerks <objektwerks@runbox.com>
2023-09-20 13:03:25 -0400

    removed logging from performance

--- src/main/scala/objektwerks/Performance.scala
+++ src/main/scala/objektwerks/Performance.scala
@@ -1,17 +1,15 @@
 package objektwerks
 
 import com.typesafe.config.ConfigFactory
-import com.typesafe.scalalogging.LazyLogging
 
 import java.util.UUID
 import java.util.concurrent.TimeUnit
 
 import org.openjdk.jmh.annotations._
 
-object Performance extends LazyLogging:
+object Performance:
   val conf = ConfigFactory.load("app.conf")
   val store = Store(conf)
-  logger.info("Database and Store initialized for performance testing.")
 
 @State(Scope.Thread)
 @BenchmarkMode(Array(Mode.AverageTime))
