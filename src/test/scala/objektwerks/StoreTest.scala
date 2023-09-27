443b97f50d26a383a7c1953c7820548dc3de6b3b
objektwerks <objektwerks@runbox.com>
2023-09-27 07:59:35 -0400

    edited store test

--- src/test/scala/objektwerks/StoreTest.scala
+++ src/test/scala/objektwerks/StoreTest.scala
@@ -12,13 +12,13 @@ final class StoreTest extends AnyFunSuite:
 
     val todo = Todo(task = "wash car")
     val id = store.addTodo(todo)
-    println(s"*** Id = $id")
+    println(s"*** Add Todo - $id")
     assert( id > 0 )
 
     val todoWithId = todo.copy(id = id)
-    println(s"*** Todo = $todoWithId")
+    println(s"*** Update Todo - $todoWithId")
     store.updateTodo(todoWithId.copy(task = "wash and dry car"))
     
     val todos = store.listTodos()
-    println(s"*** Todos = ${todos.toString}")
+    println(s"*** List Todos = ${todos.toString}")
     assert( todos.nonEmpty )
